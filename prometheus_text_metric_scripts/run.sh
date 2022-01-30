247300f51770e9a85dc33999215ec53a8c7486ff
zyr17 <jzjqz17@gmail.com>
2022-01-30 20:42:21 +0800

    update text metric scripts

--- prometheus_text_metric_scripts/run.sh
+++ prometheus_text_metric_scripts/run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+date
+
 TARGET=/var/log/prom_textfile
 
 curdir=$(readlink -f  "$(dirname "$0")")
 
-cd $curdir
+cd $curdir/scripts
 
 if [[ ! -e $TARGET ]]; then
     mkdir -p $TARGET
 fi
 
 for i in *; do
-    if [[ $i != run.sh ]]; then
+    # if [[ $i != run.sh ]]; then
         echo $i
-        $curdir/$i > $TARGET/$i.prom.tmp
+        $curdir/scripts/$i > $TARGET/$i.prom.tmp
         mv $TARGET/$i.prom.tmp $TARGET/$i.prom
-    fi
+    # fi
 done
